83b67f37e3e7956c2bfe0ab600bb0d107ff856dd
komcommy <wowivgigm09>
2019-10-29 00:27:44 +0100

    UI fixes

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -72,17 +72,19 @@
 
                     </div>
                 </div>
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <strong>DANGERZONE</strong>
-                    </div>
-                    <div class="panel-body">
-                        <p>Weet u het zeker?</p>
-                        {!! Form::open(array('class' => 'form-inline pull-right', 'method' => 'DELETE', 'route' => array('tasks.destroy', $task->id))) !!}
-                        {{ method_field('DELETE') }}
-                        {{Form::button('<span class="fa fa-trash fa-fw" aria-hidden="true"></span> <span class="hidden-xxs">Verwijderen</span> <span class="hidden-sm hidden-xs">Bestelling</span>', array('type' => 'submit', 'class' => 'btn btn-sm btn-danger'))}}
-                        {!! Form::close() !!}
-                    </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <strong>DANGERZONE</strong>
+                </div>
+                <div class="panel-body">
+                    <p>Weet u het zeker?</p>
+                    {!! Form::open(array('class' => 'form-inline pull-right', 'method' => 'DELETE', 'route' => array('tasks.destroy', $task->id))) !!}
+                    {{ method_field('DELETE') }}
+                    {{Form::button('<span class="fa fa-trash fa-fw" aria-hidden="true"></span> <span class="hidden-xxs">Verwijderen</span> <span class="hidden-sm hidden-xs">Bestelling</span>', array('type' => 'submit', 'class' => 'btn btn-sm btn-danger'))}}
+                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
