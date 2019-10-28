39b6678494522a26d82190e43f87e5ecabc91050
komcommy <wowivgigm09>
2019-10-28 23:59:50 +0100

    UI fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,7 +81,7 @@
                                 </ul>
                             </li>
 
-                            <li class="dropdown">
+                            <li class="dropdown info>">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
