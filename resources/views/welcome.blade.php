b391d76e197e79d5e6e373403efc70aa2d42bd1d
komcommy <wowivgigm09>
2019-10-28 23:14:01 +0100

    Text update & added new button.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Welkom</div>
 
                 <div class="panel-body">
+                    <a href="/login" class="btn btn-success btn-block btn-lg" role="button">Aanmelden</a>
                     <a href="/tasks-incomplete" class="btn btn-primary btn-block btn-lg" role="button">Bestellingen</a>
                 </div>
             </div>
