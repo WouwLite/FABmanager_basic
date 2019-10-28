6669c134958cd54ee82fd6c3eb812913f1c12d91
komcommy <wowivgigm09>
2019-10-28 23:17:45 +0100

    Text update & added new button.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,14 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welkom</div>
+                <div class="panel-heading">Welkom bij de Food & Beverage Manager - Basic!</div>
 
                 <div class="panel-body">
                     @if(Auth::guest())
                         <a href="/login" class="btn btn-success btn-block btn-lg" role="button">Aanmelden</a>
+                    @else
+                        <a href="/tasks-incomplete" class="btn btn-primary btn-block btn-lg" role="button">Bestellingen</a>
                     @endif()
-                    <a href="/tasks-incomplete" class="btn btn-primary btn-block btn-lg" role="button">Bestellingen</a>
                 </div>
             </div>
         </div>
